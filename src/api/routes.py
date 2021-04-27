1108a573f30bba285e4d2f87a8f4cb99b1a08724
Cristopher Ruiz <cris-nando01@hotmail.com>
2021-04-27 17:32:53 +0000

    se creo el backend del login con su token

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,12 +1,13 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+import os
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token, JWTManager, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
-
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -16,3 +17,29 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+#INICIO DE PROYECTO
+@api.route("/login", methods=["POST"])
+def login():
+    email=request.json.get("email", None)
+    password=request.json.get("password", None)
+
+    if email is None:
+        return jsonify ({"message": "Bad user or password"}),400
+    if password is None:
+        return jsonify ({"message": "Bad user or password"}),400
+    user=User.query.filter_by(email=email, password=password).first()
+    if user is None:
+        return jsonify ({"message": "Bad user or password"}),401
+    else:
+        access_token = create_access_token(identity=user.id)
+        return jsonify({"token": access_token}),200
+
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    current_user_id=get_jwt_identity()
+    user=User.query.get(current_user_id)
+    return jsonify({"id":user.id, "email":user.email})
+
+
