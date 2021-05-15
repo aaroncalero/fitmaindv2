5bdb7ebce496b034432ab944c71ee61fbf2d281c
Cristopher Ruiz <cris-nando01@hotmail.com>
2021-05-15 02:20:26 +0000

    se borro tabla calificaciones y se puso la nota mas alto y la cantidad de pruebas

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,7 +5,7 @@ import os
 #import sendgrid
 #from sendgrid.helpers.mail import *
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Calificaciones, Test, Question
+from api.models import db, User, Calificaciones, Question
 from api.utils import generate_sitemap, APIException
 from flask_mail import Message
 from flask_jwt_extended import create_access_token, JWTManager, jwt_required, get_jwt_identity
